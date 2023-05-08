b10b52c05c2a787716443827ab16c98c0a888857
Anatoliy Samoylov <a9295730700@yandex.ru>
2023-05-08 19:04:15 +0300

    added Request and CreateOrderRequest

---
+++ src/main/java/ru/praktikum/stellarburgers/model/Request.java
@@ -0,0 +1,9 @@
+package ru.praktikum.stellarburgers.model;
+
+public class Request {
+    public static final String EMAIL_POSTFIX = "@yandex.ru";
+
+    public String toJsonString() {
+        return Utils.serializeToJsonIgnoreNulls(this);
+    }
+}
